5971fb83402a40f0f5db0989d3adba8a4874d990
Florian <florian@flova.de>
2017-12-12 00:16:05 +0100

    First working implementation of integration of the Win 32 API

--- container.py
+++ container.py
@@ -4,17 +4,15 @@ class container:
         self.height = height
         self.parent = parent
         self.childs = childs
-        self.windows = windows
         self.splid_mode = splid_mode
-        self.count = count
         self.split_factor = split_factor
-        self.set_Percentage()
         self.leav = leav
         self.hwnd = hwnd
+        self.set_Percentage()
 
-    def set_Percentage():
+    def set_Percentage(self):
         for i in self.childs:
-            if self.splid_mode = 0:
+            if self.splid_mode == 0:
                 #Horrizontal Split
                 i.height = self.height
                 i.width = self.width * i.split_factor
@@ -23,29 +21,29 @@ class container:
                 i.width = self.width
                 i.height = self.height * i.split_factor
 
-    def reset_child_Percentage():
+    def reset_child_Percentage(self):
         for i in childs:
             i.split_factor = 1/self.childs.count()
         set_Percentage()
 
 
-    def change_Split_Mode():
-        if self.splid_mode == 0
+    def change_Split_Mode(self):
+        if self.splid_mode == 0:
             self.splid_mode = 1
         else:
             self.splid_mode = 0
         self.reset_child_Percentage()
 
-    def add_container(container):
+    def add_container(self,container):
         self.childs.append(container)
         set_Percentage()
 
-    def add_Part_To_Other_Childs(part_value):
+    def add_Part_To_Other_Childs(self,part_value):
         part_to_add = part_value/self.childs.count()
         for i in self.childs:
             i.split_factor += part_to_add
 
-    def kill_container(container_id):
+    def kill_container(self,container_id):
         #ToDo
         counter = 0
         for i in self.childs:
@@ -53,4 +51,4 @@ class container:
                 self.childs.pop(counter)
 
                 break
-            counter++
+            counter += 1
--- worker.py
+++ worker.py
@@ -5,13 +5,14 @@ from win32api import GetSystemMetrics
 
 system_height = GetSystemMetrics(1)
 system_width = GetSystemMetrics(0)
-outline_x = 7
-outline_y = 4
+outline_x = 0
+outline_y = 0
 
 def draw_Window(hwnd,x,y,heigth,width):
-    win32gui.MoveWindow(a, x, y, width + 2*outline_x, heigth + 2 * outline_y, True)
+    win32gui.MoveWindow(hwnd, x, y, width + 2*outline_x, heigth + 2 * outline_y, True)
 
 def place_Windows(container_uebergabe, lokation_x, lokation_y):
+    print(container_uebergabe.hwnd)
     if container_uebergabe.leav == True:
         draw_Window(container_uebergabe.hwnd,lokation_x,lokation_y,container_uebergabe.height,container_uebergabe.width)
     else:
@@ -37,15 +38,16 @@ def main():
         if win32gui.IsWindowVisible(a) and (a != 0) and str(win32gui.GetWindowText(a)) != "":
             try:
                 win32gui.ShowWindow(a, win32con.SW_NORMAL)
-                if counter == 3:
+                if counter < 3:
                     root_container.hwnd = int(a)
+                    print(win32gui.GetWindowText(a) + "  " + str(counter) + "  " + str(a))
                 counter += 1
             except Exception as e:
                 print("Error Type 1\n" + str(e))
 
-    root_container.leav = False
+    root_container.leav = True
     place_Windows(root_container,0,0)
 
 if __name__ == '__main__':
-    root_container = container(system_width,system_height, 0, [], 1, 0, 0, False,0)
+    root_container = container.container(system_width,system_height, 0, [], 1, 0, 0, False,0)
     main()
